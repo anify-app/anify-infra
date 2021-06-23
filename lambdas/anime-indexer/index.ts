a35208617f7ecd519700288f70c262ab71d9a163
Isaiah Fielder <isaiahmfielder@gmail.com>
2021-06-23 09:52:32 -0600

    add stream and indexer lambda

---
+++ lambdas/anime-indexer/index.ts
@@ -0,0 +1,37 @@
+import indexer from "algoliasearch";
+import { DynamoDBStreamEvent } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+
+export const isPresent = <T>(t: T | undefined | null | void): t is T => {
+  return t !== undefined && t !== null;
+};
+
+export const handler = async (event: DynamoDBStreamEvent) => {
+  if (!process.env.APP_ID || !process.env.INDEX_ID || !process.env.ADMIN_ID) {
+    console.error("index not configured");
+    return;
+  }
+
+  const { unmarshall } = DynamoDB.Converter;
+
+  const records = event.Records.map((record) => {
+    if (!record.dynamodb?.NewImage) {
+      return null;
+    }
+    return unmarshall(record.dynamodb?.NewImage);
+  }).filter(isPresent);
+
+  const indexedRecords = records.map((record) => ({
+    objectID: record.PK,
+    ...record,
+  }));
+
+  console.log(`found ${indexedRecords.length} records to index`);
+
+  const client = indexer(process.env.APP_ID, process.env.ADMIN_ID);
+  const index = client.initIndex(process.env.INDEX_ID);
+
+  index.saveObjects(indexedRecords).then(({ objectIDs }) => {
+    console.log(objectIDs);
+  });
+};
--- lib/pourover-infra-stack.ts
+++ lib/pourover-infra-stack.ts
@@ -1,13 +1,20 @@
 import * as cdk from "@aws-cdk/core";
-import { AttributeType, BillingMode, Table } from "@aws-cdk/aws-dynamodb";
-import { Duration } from "@aws-cdk/core";
+import {
+  AttributeType,
+  BillingMode,
+  StreamViewType,
+  Table,
+} from "@aws-cdk/aws-dynamodb";
+import { Duration, SecretValue } from "@aws-cdk/core";
 import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import { StartingPosition } from "@aws-cdk/aws-lambda";
+import { DynamoEventSource } from "@aws-cdk/aws-lambda-event-sources";
 
 export class PouroverInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // lambda function
+    // lambda function to scrape anime
     const animeScraper = new NodejsFunction(this, "AnimeScraper", {
       entry: "lambdas/anime-scraper/index.ts",
       handler: "handler",
@@ -17,8 +24,9 @@ export class PouroverInfraStack extends cdk.Stack {
     });
 
     // dynamodb table Single Table Design
-    const table = new Table(this, "AnimeDB", {
+    const animeTable = new Table(this, "AnimeDB", {
       tableName: "anime",
+      stream: StreamViewType.NEW_IMAGE,
       partitionKey: {
         name: "PK",
         type: AttributeType.STRING,
@@ -29,19 +37,49 @@ export class PouroverInfraStack extends cdk.Stack {
       },
       billingMode: BillingMode.PAY_PER_REQUEST,
     });
-    table.addGlobalSecondaryIndex({
+    animeTable.addGlobalSecondaryIndex({
       indexName: "GSI1",
       sortKey: { name: "GSI1SK", type: AttributeType.STRING },
       partitionKey: { name: "GSI1PK", type: AttributeType.STRING },
     });
 
-    table.addGlobalSecondaryIndex({
+    animeTable.addGlobalSecondaryIndex({
       indexName: "GSI2",
       sortKey: { name: "GSI2SK", type: AttributeType.STRING },
       partitionKey: { name: "GSI2PK", type: AttributeType.STRING },
     });
 
+    // lambda function to index anime
+    const animeIndexer = new NodejsFunction(this, "AnimeIndexer", {
+      entry: "lambdas/anime-indexer/index.ts",
+      handler: "handler",
+      memorySize: 4096,
+      timeout: Duration.seconds(240),
+      functionName: "anime-indexer",
+      environment: {
+        APP_ID: SecretValue.secretsManager("gh", {
+          jsonField: "APP_ID",
+        }).toString(),
+        ADMIN_ID: SecretValue.secretsManager("gh", {
+          jsonField: "ADMIN_ID",
+        }).toString(),
+        INDEX_ID: SecretValue.secretsManager("gh", {
+          jsonField: "INDEX_ID",
+        }).toString(),
+      },
+    });
+
+    animeIndexer.addEventSource(
+      new DynamoEventSource(animeTable, {
+        batchSize: 500,
+        tumblingWindow: cdk.Duration.seconds(30),
+        maxBatchingWindow: cdk.Duration.minutes(3),
+        startingPosition: StartingPosition.TRIM_HORIZON,
+      })
+    );
+    animeTable.grantStreamRead(animeIndexer);
+
     // grant lambda full operational access to table
-    table.grantFullAccess(animeScraper);
+    animeTable.grantFullAccess(animeScraper);
   }
 }

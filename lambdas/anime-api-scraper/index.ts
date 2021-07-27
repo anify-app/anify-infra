2b0fa8674bf8a63e8a7f4ac56175454694782da7
Isaiah Fielder <isaiahmfielder@gmail.com>
2021-07-27 13:50:27 -0600

    use for of for animes

--- lambdas/anime-api-scraper/index.ts
+++ lambdas/anime-api-scraper/index.ts
@@ -24,72 +24,59 @@ export const handler = async (event: event) => {
   // give status update
   console.log(`🟡 [IN PROGRESS] - (${JSON.stringify(event, null, 2)})`);
 
-  const animePromises = animeIds
-    .map(async (id) => {
-      try {
-        await delay(1000);
-        const malAnime = await JikanTS.Anime.byId(id).catch(() => undefined);
+  for (const id of animeIds) {
+    try {
+      await delay(1000);
+      const malAnime = await JikanTS.Anime.byId(id).catch(() => undefined);
 
-        if (!malAnime) {
-          return null;
-        }
-
-        const anime = {
-          title: malAnime?.title,
-          relations: determineRelation(malAnime.related),
-          description: malAnime?.synopsis,
-          trailer: malAnime?.trailer_url,
-          type: determineType(malAnime?.type || ""),
-          episodes: malAnime?.episodes,
-          status: determineStatus(malAnime?.status || ""),
-          mainImage: malAnime?.image_url,
-          rating: malAnime?.rating,
-          genres: cleanMalArrayFields(malAnime?.genres),
-          season: malAnime?.premiered?.split(" ")[0],
-          airedStart: malAnime?.aired
-            ? new Date(malAnime?.aired?.from).toISOString()
-            : null,
-          airedEnd: malAnime?.aired
-            ? new Date(malAnime?.aired?.to).toISOString()
-            : null,
-          duration: malAnime?.duration,
-          producers: cleanMalArrayFields(malAnime?.producers),
-          licensors: cleanMalArrayFields(malAnime?.licensors),
-          studios: cleanMalArrayFields(malAnime?.studios),
-          sourceMaterialType: malAnime?.source,
-          englishTitle: malAnime?.title_english,
-          japaneseTitle: malAnime?.title_japanese,
-          synonyms: malAnime?.title_synonyms,
-          sources: malAnime
-            ? [{ name: "MyAnimeList", url: malAnime?.url }]
-            : [],
-          score: malAnime?.score,
-        };
-
-        if (anime.genres.includes("Hentai")) {
-          console.log(`🔵 [SKIPPED] - Hentai detected, skipping item...`);
-          return null;
-        }
-
-        return anime;
-      } catch {
+      if (!malAnime) {
         return null;
       }
-    })
-    .filter(isPresent);
 
-  const animes = (await Promise.all(animePromises)).filter(isPresent);
+      const anime = {
+        title: malAnime?.title,
+        relations: determineRelation(malAnime.related),
+        description: malAnime?.synopsis,
+        trailer: malAnime?.trailer_url,
+        type: determineType(malAnime?.type || ""),
+        episodes: malAnime?.episodes,
+        status: determineStatus(malAnime?.status || ""),
+        mainImage: malAnime?.image_url,
+        rating: malAnime?.rating,
+        genres: cleanMalArrayFields(malAnime?.genres),
+        season: malAnime?.premiered?.split(" ")[0],
+        airedStart: malAnime?.aired
+          ? new Date(malAnime?.aired?.from).toISOString()
+          : null,
+        airedEnd: malAnime?.aired
+          ? new Date(malAnime?.aired?.to).toISOString()
+          : null,
+        duration: malAnime?.duration,
+        producers: cleanMalArrayFields(malAnime?.producers),
+        licensors: cleanMalArrayFields(malAnime?.licensors),
+        studios: cleanMalArrayFields(malAnime?.studios),
+        sourceMaterialType: malAnime?.source,
+        englishTitle: malAnime?.title_english,
+        japaneseTitle: malAnime?.title_japanese,
+        synonyms: malAnime?.title_synonyms,
+        sources: malAnime ? [{ name: "MyAnimeList", url: malAnime?.url }] : [],
+        score: malAnime?.score,
+      };
 
-  const scraperPromises = animes.map(async (anime) => {
-    await lambda
-      .invoke({
-        FunctionName: process.env.ANIME_SCRAPER as string,
-        InvocationType: "RequestResponse",
-        Payload: JSON.stringify(anime),
-      })
-      .promise();
-  });
+      if (anime.genres.includes("Hentai")) {
+        console.log(`🔵 [SKIPPED] - Hentai detected, skipping item...`);
+        return null;
+      }
 
-  const result = await Promise.all(scraperPromises);
-  return result;
+      await lambda
+        .invoke({
+          FunctionName: process.env.ANIME_SCRAPER as string,
+          InvocationType: "RequestResponse",
+          Payload: JSON.stringify(anime),
+        })
+        .promise();
+    } catch {
+      return null;
+    }
+  }
 };

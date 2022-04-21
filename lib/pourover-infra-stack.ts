37ecc3e552644557957018e1af35831ba016874c
Isaiah Fielder <isaiahmfielder@gmail.com>
2022-04-20 19:44:12 -0600

    change minutes to 5m

--- lib/pourover-infra-stack.ts
+++ lib/pourover-infra-stack.ts
@@ -182,7 +182,7 @@ export class PouroverInfraStack extends Stack {
     const stateMachineTarget = new SfnStateMachine(animeScraperStateMachine);
 
     new Rule(this, "AnimeScraperCron", {
-      schedule: Schedule.rate(Duration.minutes(3)),
+      schedule: Schedule.rate(Duration.minutes(5)),
       targets: [stateMachineTarget],
     });
   }
